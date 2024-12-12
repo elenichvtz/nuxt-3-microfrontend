95c2cf4995ef57987013b5729870f58372a91fb8
Eleni Chaviatzi <eleni@cliomusetours.com>
2024-12-12 12:31:42 +0200

    added second microfrontend app

---
+++ second-microfrontend-app/components/MyComponent.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="card">
+        This is a Card from microfrontend app 2.
+    </div>
+</template>
+
+<script setup>
+import '~/assets/scss/styles.scss';
+
+</script>
---
+++ second-microfrontend-app/nuxt.config.js
@@ -0,0 +1,48 @@
+import federation from '@originjs/vite-plugin-federation';
+
+export default defineNuxtConfig({
+  ssr: false,
+
+  css: [
+    // SCSS file in the project
+    '~/assets/scss/styles.scss'
+  ],
+
+  vite: {
+    plugins: [
+      federation({
+        name: 'microfrontend',
+        filename: 'remoteEntry.js',
+        exposes: {
+          './Component': './components/MyComponent.vue',
+        },
+      }),
+    ],
+
+    build: {
+      target: 'esnext', // Ensure ESNext target for features like dynamic imports
+    },
+
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/scss/variables/__variables-dir.scss" as *;'
+        }
+      }
+    }
+  },
+
+  build: {
+    extend(config, { isClient }) {
+      if (isClient) {
+        config.plugins.push({
+          apply: 'buildEnd',
+          enforce: 'post',
+        });
+      }
+    },
+    extractCSS: true, // Ensure CSS is extracted into separate files
+  },
+
+  compatibilityDate: '2024-12-12',
+});

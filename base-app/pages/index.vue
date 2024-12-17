fb88a4365603f4f7e744db2a41a56484e9c7d45d
Eleni Chaviatzi <eleni@cliomusetours.com>
2024-12-17 16:18:30 +0200

    added intro in base app index

--- base-app/pages/index.vue
+++ base-app/pages/index.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    <h1>Base App: Microfrontend Integration</h1>
+    <h1>Microfrontend Integration in Nuxt 3</h1>
+    
+    <p>
+      This project demonstrates a modular architecture using micro frontends, enabling the development and deployment 
+      of distinct applications that are seamlessly integrated into a central base application. 
+      The architecture promotes scalability, code reusability, and separation of concerns.
+    </p>
+
+    <p>
+      Use the sidebar to navigate to the two microfrontend apps imported in the base app of the project.
+    </p>
   </div>
 </template>
